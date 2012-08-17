2bc28b88de566de44ab876637c8e6a7a43a44e73
simbirsk <albertegz@gmail.com>
2012-08-17 13:27:22 +0200

    force user + location in review build

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,10 @@
 class Review < ActiveRecord::Base
   belongs_to :location
   belongs_to :user
-  attr_accessible :body
+
+  attr_accessible :body, :user_id, :location_id
+
+  validates :body, :presence => true
+  validates :user_id, :presence => true
+  validates :location_id, :presence => true
 end
