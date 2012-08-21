a2008e08a4987885aac71887a68084df7c064b66
simbirsk <albertegz@gmail.com>
2012-08-21 11:33:03 +0200

    forbid guests everything except showing

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,9 @@
 class HomeController < ApplicationController
   def index
-
     @json = Location.all.to_gmaps4rails do |location, marker|
       marker.infowindow render_to_string(:partial => "/locations/infowindow", :locals => { :object => location})
       marker.json({ :id => location.id })
     end
     @location = Location.new
-
   end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,6 @@
 class ReviewsController < ApplicationController
+  before_filter :authenticate_user!
+
   def create
     @location = Location.find(params[:location_id])
     @review = @location.reviews.create(params[:review])
